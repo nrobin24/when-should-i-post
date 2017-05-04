20892fba3664f42212085b11da3d93d67f40fb86
Nick Robin <xb@Nicholass-MacBook-Pro.local>
2017-05-03 23:48:10 -0700

    add some components

---
+++ components/Page.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import Link from 'next/link'
+import { inject, observer } from 'mobx-react'
+import Clock from './Clock'
+import SubredditSelector from './subredditSelector'
+
+@inject('store') @observer
+class Page extends React.Component {
+  componentDidMount () {
+    this.props.store.start()
+  }
+
+  componentWillUnmount () {
+    this.props.store.stop()
+  }
+
+  render () {
+    return (
+      <div>
+        <h1>{this.props.title}</h1>
+        <SubredditSelector />
+        <img src="/images/hist_0.png"></img>
+        <Clock lastUpdate={this.props.store.lastUpdate} light={this.props.store.light} />
+        <nav>
+          <Link href={this.props.linkTo}><a>Navigate</a></Link>
+        </nav>
+      </div>
+    )
+  }
+}
+
+export default Page
