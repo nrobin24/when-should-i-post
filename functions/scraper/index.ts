32f3e7760f8fb753b90e8ac8de9c2408c1a1f0bc
Nick Robin <xb@Nicholass-MacBook-Pro.local>
2017-05-14 23:04:41 -0700

    switch to dynamo

--- functions/scraper/index.ts
+++ functions/scraper/index.ts
@@ -4,19 +4,33 @@ import * as R from 'ramda'
 
 const snoowrap = require('snoowrap')
 const credentials : Credentials = require('./credentials')
-const promisify = require('promisify-node')
 
-const s3 = new AWS.S3()
-const s3Bucket = 'nr-wsip'
+const dynasty = require('dynasty')(credentials.dynamo);
 
-import {putObject, getObject, listKeys} from './aws-utils'
-// const getObject = promisify(s3.getObject)
-
+const indexTableName = 'nr-wsip-posts_index'
+const dataTableName = 'nr-wsip-posts_data'
 
 interface Credentials {
   clientId: string;
   clientSecret: string;
   refreshToken: string;
+  userAgent: string;
+  dynamo: {
+    accessKeyId: string;
+    secretAccessKey: string;
+    region: string;
+  }
+}
+
+interface Post {
+  id: string;
+  created: number;
+  subreddit: string;
+  ups: number;
+  title: string;
+  author: any;
+  selftext: string;
+  permalink: string;
 }
 
 export async function handle(e, ctx, cb) {
@@ -26,50 +40,86 @@ export async function handle(e, ctx, cb) {
     const today = moment().format('YYYY-MM-DD')
     console.log('today is: ', today)
 
-    console.log("checking for daily data file")
-    const keys = await listKeys(s3Bucket, today)
-    const todayKeyExists = keys.filter(R.equals(today))
-
-    let posts = []
-    if (todayKeyExists.length) {
-      const oldPosts = await getObject(s3Bucket, today)
-      posts = posts.concat(oldPosts)
-    }
-    console.log(`already keeping track of ${posts.length} posts`)
-
     // Get data from reddit
     const {clientId, clientSecret, refreshToken} = credentials
     const r = new snoowrap({
-      userAgent: 'wsip_scraper',
+      userAgent: credentials.userAgent,
       clientId,
       clientSecret,
       refreshToken
     })
 
     const subs : String[] = await r.getPopularSubreddits({limit: 20}).map(s => s.display_name)
-    const newPostsNested = await Promise.all(subs.map(s => r.getSubreddit(s).getNew({limit: 5})))
+    const newPostsNested = await Promise.all(subs.map(s => r.getSubreddit(s).getNew({limit: 50})))
+
     const newPostsData = newPostsNested.reduce((p, c) => p.concat(c),[])
 
-    const newPosts = newPostsData.map(p => {
-      const {id, created, subreddit, ups} = p
+    const newPosts: Post[] = newPostsData.map(p => {
+      const {id, created, subreddit, ups, title, author, selftext, permalink} = p
       const lastUpdate = moment().unix()
-      return {id, created, subreddit: subreddit.display_name, lastUpdate, ups}
+      return {id, created, subreddit: subreddit.display_name, title, author, selftext, permalink}
     })
 
-    // Add any unseen posts to existing data
-    console.log(`fetched info from reddit on ${newPosts.length} posts`)
-    const allPosts = R.uniqBy(R.prop('id'), R.concat(posts, newPosts))
 
-    const newPostsFound = allPosts.length - posts.length
-    console.log(`${newPostsFound} new posts found`)
+    // upsert post ids in index table
+    const indexTable = dynasty.table(indexTableName)
+    const dataTable = dynasty.table(dataTableName)
 
-    // Write data to file in S3
-    if (newPostsFound) {
-      console.log("putting posts to s3")
-      const body: any = allPosts.reduce((p: string, c: string) => p + JSON.stringify(c) + '\n', "")
-      const s3Response = await putObject(s3Bucket, today, body)
-      console.log("got s3 response", s3Response)
+    const toIndexUpdate = (post: Post) => indexTable.update(
+      {hash: today, range: post.created}, {post_id: post.id}
+    )
+
+    console.log(`updating index table with ${newPosts.length} posts`)
+    const updateIndexResp = await Promise.all(newPosts.map(toIndexUpdate))
+
+    console.log(`got update index table response, non empty responses:`)
+    const nonEmptyIndexResps = updateIndexResp.filter(r => !!Object.keys(r).length)
+    console.log(nonEmptyIndexResps)
+
+    // upsert other fields in data table
+    const now = parseInt(moment().format('X'))
+
+    const toDataUpdate = (post: Post) => {
+      let {subreddit, id, title, author, selftext, permalink} = post
+
+      // deal with special cases of author field
+      if (typeof author == 'object' && author.name) {
+        author = author.name
+      } else if (typeof author == 'object' && author.RedditUser && author.RedditUser.name) {
+        author = author.RedditUser.name
+      }
+      if (typeof author != 'string') {
+        author = JSON.stringify(author)
+      }
+
+      // deal with empty selftext field
+      if (selftext == "") {
+        selftext = "emptyString"
+      }
+
+      const fields = ['subreddit', 'id', 'author', 'selftext', 'permalink']
+      for (let field of fields) {
+        if (typeof post[field] == 'undefined' && post[field]) {
+          console.log(`post was skipped because of undefined field ${field}`)
+          console.log('post:', post)
+          return Promise.resolve({})
+        }
+      }
+      return dataTable.update(id, {subreddit, title, author, selftext, permalink, lastUpdated: now}).catch(e => {
+        console.log('error while processing post')
+        console.log(post)
+        throw new Error(e)
+      })
     }
+
+    console.log(`updating data table`)
+    const updateDataResp = await Promise.all(newPosts.map(toDataUpdate))
+
+    console.log(`got update data table response`)
+    console.log(`got update data table response, non empty responses:`)
+    const nonEmptyDataResps = updateDataResp.filter(r => !!Object.keys(r).length)
+    // const nonEmptyDataResps = updateDataResp
+    console.log(nonEmptyDataResps)
 
     cb(null, 'success')
   } catch (e) {
--- functions/updater/index.ts
+++ functions/updater/index.ts
@@ -1,9 +1,55 @@
 import * as AWS from 'aws-sdk'
-const s3 = new AWS.S3()
-const s3Bucket = 'nr-wsip'
+import * as moment from 'moment'
+import * as R from 'ramda'
+
+const fetch = require('node-fetch')
+
+const snoowrap = require('snoowrap')
+
+const credentials = require('credentials')
+const dynasty = require('dynasty')(credentials.dynamo);
+
+const indexTableName = 'nr-wsip-posts_index'
+const dataTableName = 'nr-wsip-posts_data'
 
 export async function handle(e, ctx, cb) {
   try {
+
+
+    const postIds = await getPostIds()
+
+    console.log('post ids sample')
+
+    console.log(R.take(10, postIds))
+
+    const batches = R.splitEvery(20, postIds)
+
+    console.log(`found ${postIds.length} posts`)
+
+    // Get data from reddit
+    // const {clientId, clientSecret, refreshToken} = credentials
+    // const r = new snoowrap({
+    //   userAgent: credentials.userAgent,
+    //   clientId,
+    //   clientSecret,
+    //   refreshToken
+    // })
+
+    // const postsNested = await Promise.all(batches.map(b => r.getSubmission(R.pluck('post_id', b))))
+    // const posts = R.unnest(postsNested)
+
+    const baseUrl = `https://www.reddit.com`
+    let url = baseUrl + `/by_id/` + batches[0].map(i => 't3_' + i).join(',') + '.json'
+    console.log('url', url)
+    const posts = await fetch(url).then(r => r.json()).then(r => r.data.children)
+
+    console.log(`got ${posts.length} posts`)
+
+    console.log(`updating data table`)
+    const postsUpdateResp = await Promise.all(posts.filter(p => p.id && p.score).map(p => updateScore(p.id, p.score)))
+
+    // console.log(`got data for ${posts.length} posts`)
+    // console.log(posts)
 
     cb(null, 'done')
   } catch (e) {
@@ -12,7 +58,14 @@ export async function handle(e, ctx, cb) {
   }
 }
 
-function readS3Files() {
+const indexTable = dynasty.table(indexTableName)
+const dataTable = dynasty.table(dataTableName)
 
+function getPostIds(): Promise<string[]> {
+  const today = moment().format('YYYY-MM-DD')
+  return indexTable.findAll(today).then(ps => ps.map(p => p.post_id))
+}
 
+function updateScore(post_id: string, score: number): Promise<any> {
+  return dataTable.update(post_id, {score})
 }
