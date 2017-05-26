66aa3e12f171ba9419401149877efc6256584f18
Nick Robin <nrobin@cs-nick-mbp.local>
2017-05-26 08:20:31 -0700

    Updates

---
+++ _next/af700216-72f3-4a05-bbe3-c58b98f55c3a/page/index.js
@@ -0,0 +1,6 @@
+
+          window.__NEXT_REGISTER_PAGE('/', function() {
+            var comp = module.exports=webpackJsonp([2],[],[1666]);
+            return { page: comp.default }
+          })
+        
